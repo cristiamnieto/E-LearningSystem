2ef0c1c5e8ae58462ce9975773f1ad28b320e6bf
Alejandro1722 <66704330+Alejandro1722@users.noreply.github.com>
2020-06-23 16:39:50 -0500

    Add files via upload

--- E-LearningSystem/lesson.php
+++ E-LearningSystem/lesson.php
@@ -1,57 +1,62 @@
 <h1><?php echo $title;?></h1>
-<div class="col-lg-6">
-	<h3>PDF</h3>
-	<div class="table-responsive">
-		<table id="example" class="table table-bordered">
-			<thead>
-				<th width="2%">#</th>
-				<th>Chapter</th>
-				<th>Title</th> 
-				<th width="2%">Action</th>
-			</thead>
-			<tbody>
-				<?php 
-				$sql = "SELECT * FROM tbllesson WHERE Category='Docs'";
-				$mydb->setQuery($sql);
-				$cur = $mydb->loadResultList();
-				foreach ($cur as $result) {
-					# code...
-					echo '<tr>';
-					echo '<td></td>';
-					echo '<td>'.$result->LessonChapter.'</td>';
-					echo '<td>'.$result->LessonTitle.'</td>';
-					echo '<td><a href="index.php?q=viewpdf&id='.$result->LessonID.'" class="btn btn-xs btn-info"><i class="fa fa-info"></i> View File</a></td>';
-					echo '</tr>';
-				}
-				?>
-			</tbody>
-		</table>
+
+<link rel="stylesheet" type="text/css" href="css/newstyle.css">
+
+<div class="leccion">
+	<div class="col-lg-6" id="col-lg-6">
+		<h3>PDF</h3>
+		<div class="table-responsive" id="table-responsive">
+			<table id="example" class="table table-bordered">
+				<thead>
+					<th width="2%">#</th>
+					<th>Chapter</th>
+					<th>Title</th> 
+					<th width="2%">Action</th>
+				</thead>
+				<tbody>
+					<?php 
+					$sql = "SELECT * FROM tbllesson WHERE Category='Docs'";
+					$mydb->setQuery($sql);
+					$cur = $mydb->loadResultList();
+					foreach ($cur as $result) {
+						# code...
+						echo '<tr>';
+						echo '<td></td>';
+						echo '<td>'.$result->LessonChapter.'</td>';
+						echo '<td>'.$result->LessonTitle.'</td>';
+						echo '<td><a href="index.php?q=viewpdf&id='.$result->LessonID.'" class="btn btn-xs btn-info"><i class="fa fa-info"></i> View File</a></td>';
+						echo '</tr>';
+					}
+					?>
+				</tbody>
+			</table>
+		</div>
 	</div>
-</div>
-<div class="col-lg-6">
-	<h3>VIDEO</h3>
-	<div class="table-responsive">
-		<table id="example2" class="table table-bordered">
-			<thead>
-				<th width="2%">#</th>
-				<th>Decription</th>
-				<th width="2%">Action</th>
-			</thead>
-			<tbody>
-				<?php 
-				$sql = "SELECT * FROM tbllesson WHERE Category='Video'";
-				$mydb->setQuery($sql);
-				$cur = $mydb->loadResultList();
-				foreach ($cur as $result) {
-					# code...
-					echo '<tr>';
-					echo '<td></td>';
-					echo '<td>'.$result->LessonTitle.'</td>'; 
-					echo '<td><a href="index.php?q=playvideo&id='.$result->LessonID.'" class="btn btn-xs btn-info"><i class="fa fa-play"></i> Play Video</a></td>';
-					echo '</tr>';
-				}
-				?>
-			</tbody>
-		</table>
+	<div class="col-lg-6" id="video">
+		<h3>VIDEO</h3>
+		<div class="table-responsive">
+			<table id="example2" class="table table-bordered">
+				<thead>
+					<th width="2%">#</th>
+					<th>Decription</th>
+					<th width="2%">Action</th>
+				</thead>
+				<tbody>
+					<?php 
+					$sql = "SELECT * FROM tbllesson WHERE Category='Video'";
+					$mydb->setQuery($sql);
+					$cur = $mydb->loadResultList();
+					foreach ($cur as $result) {
+						# code...
+						echo '<tr>';
+						echo '<td></td>';
+						echo '<td>'.$result->LessonTitle.'</td>'; 
+						echo '<td><a href="index.php?q=playvideo&id='.$result->LessonID.'" class="btn btn-xs btn-info"><i class="fa fa-play"></i> Play Video</a></td>';
+						echo '</tr>';
+					}
+					?>
+				</tbody>
+			</table>
+		</div>
 	</div>
 </div>
