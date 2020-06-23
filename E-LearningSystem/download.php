cb7441fe4b11413a2460ca119c489efdbdd371e2
Alejandro1722 <66704330+Alejandro1722@users.noreply.github.com>
2020-06-23 16:04:53 -0500

    Add files via upload

--- E-LearningSystem/download.php
+++ E-LearningSystem/download.php
@@ -1,16 +1,20 @@
 <h1><?php echo $title;?></h1>
-<div class="col-lg-6">
-	<h3>PDF</h3>
-	<div class="table-responsive">
-		<table id="example" class="table table-bordered">
-			<thead>
-				<th width="2%">#</th>
-				<th>Chapter</th>
-				<th>Title</th> 
-				<th width="10%">Action</th>
-			</thead>
-			<tbody>
-				<?php 
+
+<link rel="stylesheet" type="text/css" href="css/newstyle.css">
+
+<div class="contenedor">
+	<div class="col-lg-6">
+		<h3>PDF</h3>
+		<div class="table-responsive">
+			<table id="example" class="table table-bordered">
+				<thead>
+					<th width="2%">#</th>
+					<th>Chapter</th>
+					<th>Title</th>
+					<th width="10%">Action</th>
+				</thead>
+				<tbody>
+					<?php 
 				$sql = "SELECT * FROM tbllesson WHERE Category='Docs'";
 				$mydb->setQuery($sql);
 				$cur = $mydb->loadResultList();
@@ -24,21 +28,21 @@
 					echo '</tr>';
 				}
 				?>
-			</tbody>
-		</table>
+				</tbody>
+			</table>
+		</div>
 	</div>
-</div>
-<div class="col-lg-6">
-	<h3>VIDEO</h3>
-	<div class="table-responsive">
-		<table id="example2" class="table table-bordered">
-			<thead>
-				<th width="2%">#</th>
-				<th>Decription</th>
-				<th width="10%">Action</th>
-			</thead>
-			<tbody>
-				<?php 
+	<div class="col-lg-6" id="video">
+		<h3>VIDEO</h3>
+		<div class="table-responsive">
+			<table id="example2" class="table table-bordered">
+				<thead>
+					<th width="2%">#</th>
+					<th>Decription</th>
+					<th width="10%">Action</th>
+				</thead>
+				<tbody>
+					<?php 
 				$sql = "SELECT * FROM tbllesson WHERE Category='Video'";
 				$mydb->setQuery($sql);
 				$cur = $mydb->loadResultList();
@@ -51,7 +55,8 @@
 					echo '</tr>';
 				}
 				?>
-			</tbody>
-		</table>
+				</tbody>
+			</table>
+		</div>
 	</div>
 </div>
