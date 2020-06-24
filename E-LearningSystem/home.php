32c408015ce8bf478565cb52f7327bb47f713693
Juan Sebastian Camargo Prieto <51170208+ImSebz@users.noreply.github.com>
2020-06-23 21:00:00 -0500

    Update home.php

--- E-LearningSystem/home.php
+++ E-LearningSystem/home.php
@@ -1,57 +1,36 @@
 <!-- <h1><?php echo $title;?></h1> -->
 
 <div class="col-lg-12">  
-<h4>Mandate</h4>
-    <p> The Capiz State University (CAPSU) primarily provides advance instruction and professional training in agriculture, fishery and forestry, science and technology, arts and humanities, education and other related fields. It shall also undertake research, extension services and production activities and provide progressive leadership in its areas of specialization.</p> 
+<h3>POLITECNICO INTERNACIONAL</h3>
+  <img src="images/Politecnicoint.jpg" width="20%" height="20%"> 
+    <p> Somos un grupo por empresarios que creen en Colombia, deseosos de retribuir a la sociedad y al país los rendimientos económicos originados por sus actividades. El Politécnico Internacional es una Institución de Educación Superior constituida como una fundación privada sin ánimo de lucro, la cual <strong>contribuye a la formación integral de la juventud colombiana, para hacer realidad sus sueños a través de la empleabilidad o el emprendimiento.</strong></p>
+    <p><strong>Es una Institución comprometida con el desarrollo laboral</strong> de sus egresados y es reconocida por las destacadas competencias prácticas y por los altos estándares de calidad y servicio que desarrolla en sus estudiantes; los cuales tienen una gran acogida en el sector productivo.</p> 
 </div>
 <div  class="col-lg-6">
-  <h4>Vision</h4> 
-  <p>Center of Academic Excellence Delivering Quality Service to all.</p>
-  <h4>Mission</h4>
-  <p> Capiz State University is committed to provide advanced knowledge and innovation; develop skills, talents and values; undertake relevant research; development and extension services; promote entrepreneurship and environmental consciousness; and enhance industry collaboration  & linkages with partner agencies.</p>
-  <h4>Goals</h4> 
+  <h3>Vision</h3> 
+  <h4>Lo que nos diferencia</h4>
+  <p> <strong>Modelo decentralizado</strong>, la ubicación estratégica de nuestras cuatro sedes facilita a los jóvenes poder ir a estudiar sin incurrir en mayores gastos, debido a la cercanía de estas a sus lugares de vivienda o trabajo. Desde el 2019 implementamos nuestra modalidad virtual, dando más cobertura y llegando a más personas a nivel nacional.</p>
+  <h4>Política de Calidad</h4> 
   <ol>
-    <li>Globally competitive graduates</li>  
-    <li>Institutionalize research culture</li> 
-    <li>Responsive and sustainable extension services</li> 
-    <li>Maximized profit of variable agro-industrial business ventures</li>  
-    <li>Effective and efficient administration </li>  
-  </ol>
-
-  <h4>Objectives</h4> 
-   <ol>
-      <li>Create an environment of shared leadership and responsibilities with competent administrator.</li>
-      <li>Provide relevant trainings and seminar to faculty, staff, and student.</li>
-      <li>Produce highly competitive graduates.</li>
-      <li>Conduct relevant and updated researches</li>
-      <li>Extend financial support and manpower for outreach activities.</li> 
-   </ol> 
+    <li>La excelencia educativa a través de la pertinencia de los programas académicos.</li>  
+    <li>Instalaciones impecables y pertinentes para satisfacer las necesidades del proceso formativo.</li> 
+    <li>Docentes idóneos que se caracterizan porque saben de lo que enseñan y motivan el aprendizaje.</li> 
+    <li>Procesos administrativos eficientes y eficaces para satisfacer a toda la comunidad</li>  
+  </ol>  
 </div>
 <div  class="col-lg-6">
-  <h4>Guiding Principles</h4> 
-    <ul>
-      <li>Academic Freedom </li>
-      <li>Responsibility</li>
-      <li> Academic Standards</li>  
-    </ul>
-<h4>Core Values</h4>
-  <ul>
-    <li>God-Centered</li>
-    <li>Excellence</li>
-    <li>Integrity</li>
-    <li>Transparency and Accountability</li>
-    <li>Dedication to Quality Service</li>  
-  </ul> 
-<h4>Quality Policy</h4> 
-<ol>
-    <li>CAPSU is committed to be the center of academic excellence delivering quality service to all by;</li> 
-    <li>Continuing innovations and quality improvement cultivating an efficient and effective and environment for maximum clientele satisfaction;</li> 
-    <li>Adhering to laws and regulations, global standards, and environmental change requirements;</li> 
-    <li>Participating in sustainable developments projects for inclusive economic for inclusive 
-economic growth;</li> 
-    <li>Showcasing quality outputs; and,</li> 
-    <li>Upholding values and integrity and nurturing talents and skills for global competitiveness.</li>  
-</ol>
+<h4>Objetivos de Calidad</h4> 
+   <ul>
+      <li>Ofrecer nuevos programas requeridos en mercado laboral</li>
+      <li>Crear la Unidad de Educación Virtual.</li>
+      <li>Fortalecer el desarrollo profesoral para garantizar la calidad de enseñanza.</li>
+      <li>Promover la cultura de investigación</li>
+      <li>Estimular el aprendizaje de una Segunda lengua</li>
+      <li>Ser una Institución de Carácter Tecnológico</li> 
+      <li>Brindar procesos academico-administrativos eficases y eficientes</li> 
+      <li>Proporcionar espacios físicos y tecnológicos idóneos para el aprendizaje</li> 
+      <li>Incentivar las relaciones internacionales</li> 
+   </ul> 
 </div>
  
 
